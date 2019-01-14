2bec32171c67af133a2d9fa7a9e3ce66bba2e6ce
Emilis Belte <emilis.belte@gmail.com>
2019-01-14 21:35:16 +0000

    change db url

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ var commentRoutes   = require("./routes/comments"),
 // seedDB();
 
 //CONNECTS A DATABASE OR CREATES IT IF IT DOES NOT EXIST
-mongoose.connect("mongodb://bonno:EDFDf3eb9cc0@ds157204.mlab.com:57204/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://bonno:EDFDf3eb9cc0@ds157204.mlab.com:57204/yelpcamp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
